9a830de1a8dca192d71cb166328c10bacc8f8ed5
Nadav Ivgi <nadav@shesek.info>
2016-09-13 05:10:28 +0300

    refactoring

--- client.js
+++ client.js
@@ -1,59 +1,50 @@
-import Rx, { Observable as O } from 'rx';
-import Cycle from '@cycle/core';
-import { makeDOMDriver, div, ul, li, span, hr, pre, h1, h2, h3, p, button, br } from '@cycle/dom';
+import Rx, { Observable as O } from 'rx'
+import Cycle from '@cycle/core'
+import { makeDOMDriver, div, pre, button, br, p, small } from '@cycle/dom'
 import io from 'socket.io-client'
-import { dbg } from './util'
+import dbg from './dbg-stream'
 
-window.mdebug = require('debug')('LnCC')
+const makeSocketDriver = socket => out$ => (
+  out$.subscribe(a => socket.emit(...a)),
+  { events: O.fromEvent.bind(O, socket) })
 
-const merge = (...xs) => Object.assign({}, ...xs)
-const byName = name => x => x.name === name
+const main = ({ DOM, socket, props$ }) => {
+  const
 
-let main = ({ DOM, socket, props$ }) => {
-
-  let log$ = socket.events('log')
+  // Filter for events of `name`for the currently active client
+  valOf = name => socket.events('log')
     .withLatestFrom(props$)
-    .filter(([ log, props ]) => log.client == props.client)
-    .map(([ log, _ ]) => log)
-  dbg('log$', log$)
+    .flatMap(([ ev, p ]) => ev[0] == p.client && ev[1] == name ? O.just(ev[2]) : O.empty())
 
   // Model
-  let channelPoint$ = log$.filter(byName('channelpoint'))
-    .map(({ txid, index }) => ({ txid, index  }))
-    .startWith({ txid: 'N/A', index: 'N/A' })
-  let blockHeight$ = log$.filter(byName('block')).map(l => l.height).startWith('N/A')
-  let balance$ = log$.filter(byName('balance')).startWith({ ourBalance: 'N/A', theirBalance: 'N/A' })
+, point$   = valOf('outpoint').map(point => `${point.txid.substr(0,10)}:${point.index}`).startWith(undefined)
+, height$  = valOf('height').startWith(undefined)
+, bench$   = valOf('benchmark').withLatestFrom(props$, (b, p) => `${b.tps} tx/sec: sent ${b.sent} ${p.asset}, received ${b.recv} ${p.asset} in the last ${b.timeframe}`).startWith(undefined)
+, balance$ = valOf('balance').startWith(undefined)
 
   // Intent
-  let pay$ = DOM.select('button.pay').events('click')
-    .map(ev => ({ action: 'pay', amount: ev.target.value }))
-
-  let settle$ = DOM.select('button.settle').events('click')
-    .withLatestFrom(channelPoint$, (_, channelPoint) => ({ action: 'settle', channelPoint }))
-
-  let actions$ = O.merge(pay$, settle$)
-    .withLatestFrom(props$, (action, props) => [ 'rpc', merge(action, { client: props.client }) ])
+, pay$    = DOM.select('.pay').events('click').map(ev => [ 'pay', ev.target.value ])
+, settle$ = DOM.select('.settle').events('click').withLatestFrom(point$, (_, point) => [ 'settle', point ])
+, out$    = O.merge(pay$, settle$).withLatestFrom(props$, (action, p) => [ 'rpc', p.client, ...action ])
 
   // View
-  let vtree$ = O.combineLatest(blockHeight$, channelPoint$, balance$)
-    .map(([ blockHeight, channelPoint, balance ]) => div([
-      pre(JSON.stringify({ channelPoint: channelPoint, blockHeight: blockHeight, ourBalance: balance.ourBalance, theirBalance: balance.theirBalance }, '\t', 2)),
-      button('.pay', { value: 1 }, 'Send 1 CC'), ' ',
-      button('.pay', { value: 5 }, 'Send 5 CC'), ' ',
-      button('.pay', { value: 10 }, 'Send 10 CC'), ' ',
-      br(), br(),
-      button('.settle', { }, 'Settle on-chain'), ' ',
+, vtree$ = O.combineLatest(point$, height$, balance$, bench$, props$)
+    .map(([ point, height, balance, bench, props ]) => div([
+     balance ? pre(JSON.stringify(balance, '\t', 2)) : null
+    , [ 1, 5, 10 ].map(value => [ button('.pay', { value }, `Send ${value} ${props.asset}`), ' ' ])
+    , br(), br()
+    , button('.settle', 'Settle on-chain')
+    , (point && height) ? [' ', small(null, `commitment #${ height } on ChannelPoint(${ point }) `) ] : null
+    , bench ? [ br(), p(small(null, `benchmark: ${bench}`))] : null
     ]))
 
-  return { DOM: vtree$, socket: actions$ }
+  // Sinks
+  dbg({ point$, height$, balance$, bench$, out$ })
+  return { DOM: vtree$, socket: out$ }
 }
 
-let makeSocketDriver = socket => out$ => (
-  out$.subscribe(a => socket.emit(...a)),
-  { events: name => O.fromEvent(socket, name) }
-)
 Cycle.run(main, {
   DOM: makeDOMDriver('#app')
 , socket: makeSocketDriver(io(location.origin, { transports: [ 'websocket' ] }))
-, props$: _ => O.just({ client: location.hash.substr(1) })
+, props$: _ => O.just({ client: location.hash.substr(1), asset: 'CC' })
 })
--- server.js
+++ server.js
@@ -3,79 +3,69 @@ import socketio from 'socket.io'
 import http from 'http'
 import browserify from 'browserify-middleware'
 import Rx, { Observable as O } from 'rx'
-import RxNode from 'rx-node'
+import RxNode, { fromStream } from 'rx-node'
 import { throwerr, printerr } from 'iferr'
 import grpc from 'grpc'
 import lnrpc from 'lnrpc'
 import logstream from 'lnd-logstream'
-import { dbg } from './util'
+import dbg from './dbg-stream'
 
-require('longjohn')
-
-const debug = require('debug')('LnCC')
+const toChannelPoint = ({ txid, index }) => new ChannelPoint(new Buffer(txid, 'hex'), +index)
     , { Lightning, SendRequest, CloseChannelRequest, ChannelPoint, GetInfoRequest } = lnrpc
-    , toChannelPoint = ({ txid, index }) => new ChannelPoint(new Buffer(txid, 'hex'), +index)
-    , merge = (...xs) => Object.assign({}, ...xs)
 
 // Setup LN RPC clients
 const lnClients = {
   bob:   new Lightning(process.env.BOB_LNRPC, grpc.credentials.createInsecure())
-, alice: new Lightning(process.env.ALICE_LNRPC, grpc.credentials.createInsecure())
-}, nodeIds = {}, otherMap = { bob: 'alice', alice: 'bob' }, sendCalls={}
+, alice: new Lightning(process.env.ALICE_LNRPC, grpc.credentials.createInsecure()) }
+;((a, b) => (a._other=b, b._other=a))(lnClients.bob, lnClients.alice)
 
 // Load lightning_id for all clients
 Object.keys(lnClients).forEach(
     k => lnClients[k].getInfo(new GetInfoRequest, throwerr(
-        info => nodeIds[k] = new Buffer(info.lightning_id, 'hex'))))
+        info => lnClients[k]._lightning_id = new Buffer(info.lightning_id, 'hex'))))
 
-// Setup log streams
-const lnStreams = {
-  bob:   logstream(process.env.BOB_LOG)
-, alice: logstream(process.env.ALICE_LOG)
-}
+// Setup a merged log stream for all clients, prepending the source client to the event tuple
+const log$ = O.merge(
+  logstream(process.env.BOB_LOG).map(ev => [ 'bob', ...ev ])
+, logstream(process.env.ALICE_LOG).map(ev => [ 'alice', ...ev ]))
 
-const log$ = O.merge(...Object.keys(lnStreams).map(client => lnStreams[client].map(x => merge(x, { client }))))
-dbg('log$', log$)
-
-// Express
+// Express-based HTTP interface
 const app = express()
+    , server = http.Server(app)
 
 app.set('port', process.env.PORT || 9000)
 app.set('view engine', 'jade')
+app.set('views', __dirname + '/views')
 
 app.use(require('morgan')('dev'))
 
-app.get('/', (req, res) => res.render(__dirname + '/views/index'))
+app.get('/', (req, res) => res.render('index'))
 app.get('/_.js', browserify(__dirname + '/client.js'))
 app.use('/_', express.static(__dirname + '/static'))
 
-const server = http.Server(app)
-
-// RPC action handlers
-const rpcActions = {
-  pay: (client, req) => {
-    let isNew = false, call = sendCalls[req.client] || (isNew = true, sendCalls[req.client] = client.sendPayment())
-    call.write(new SendRequest(nodeIds[otherMap[req.client]], +req.amount))
-    return isNew ? RxNode.fromStream(call) : O.empty()
-  }
-, settle: (client, req) => {
-    const request = new CloseChannelRequest(toChannelPoint(req.channelPoint))
-    return RxNode.fromStream(client.closeChannel(request))
-  }
-}
-
-// Socket.io
+// Socket.io-based WebSocket interface
 const io = socketio(server)
 io.set('transports', ['websocket'])
 io.addEventListener = io.on // hack for RxJS compatibility
 
-const rpcRequest$ = O.fromEvent(io, 'connection').flatMap(socket => O.fromEvent(socket, 'rpc'))
-const rpcReply$ = rpcRequest$.flatMap(req => rpcActions[req.action](lnClients[req.client], req))
+// RPC actions
+const rpcActions = {
+  pay: (c, amount) => {
+    let isNew = false, stream = c._sendStream || (isNew = true, c._sendStream = c.sendPayment())
+    stream.write(new SendRequest(c._other._lightning_id, +amount))
+    return isNew ? fromStream(stream) : O.empty()
+  }
+, settle: (client, point) => fromStream(client.closeChannel(new CloseChannelRequest(toChannelPoint(point))))
+}
 
-dbg('rpcRequest$', rpcRequest$)
-dbg('rpcReply$', rpcReply$)
+// make a stream of incoming RPC requests, execute them and transform to a stream of RPC responses
+const rpcRequest$ = O.fromEvent(io, 'connection').flatMap(socket => O.fromEvent(socket, 'rpc', (...a) => a)).share()
+    , rpcReply$   = rpcRequest$.flatMap(([ client, action, ...a ]) => rpcActions[action](lnClients[client], ...a)).share()
 
-log$.subscribe(x => io.emit('log', x))
+// Announce ln log events & RPC replies to all clients (they're responsible for filtering the ones they care about)
+log$.subscribe(ev => io.emit('log', ev))
+rpcReply$.subscribe(r => io.emit('rpcReply', r))
 
-// Go
+// Launch
 server.listen(app.get('port'), _ => console.log(`Listening on ${app.get('port')}`))
+dbg({ /*log$,*/ rpcRequest$, rpcReply$ })
