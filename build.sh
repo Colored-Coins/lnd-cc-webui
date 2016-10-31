616eb67d1c0beb798aa9567722dafabd0eefabe7
Nadav Ivgi <nadav@shesek.info>
2016-10-31 11:21:28 +0200

    minify js

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 
 source .env
 
-browserify --entry client.js > static/-.js
+browserify --entry client.js | uglifyjs --mangle --compress warnings=false > static/-.js
 stylus --compress < style.styl > static/-.css
 jade --no-debug -O '{"url":"'"$URL"'", "static_url":"'"$STATIC_URL"'", "version": "'"$VER"'"}' < index.jade > static/index.html
 
