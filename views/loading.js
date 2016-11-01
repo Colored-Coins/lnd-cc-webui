4412a615f8b575017923c17a0cb4defaf642bc20
Nadav Ivgi <nadav@shesek.info>
2016-11-01 23:56:49 +0200

    change loading texxt

--- views/loading.js
+++ views/loading.js
@@ -2,5 +2,5 @@ import { header, div, h2, p, span } from '@cycle/dom'
 
 module.exports = (balance, asset, total_locked) => (console.log('render loading....'),
   header('.overview', [ div('.container', [
-    h2('Setting up wallet...')
+    h2('Setting up Lightning wallet...')
   ]) ]))
