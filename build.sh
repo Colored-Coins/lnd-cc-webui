4e00356d39f960442786d697625e67de12fd3a05
Nadav Ivgi <nadav@shesek.info>
2016-11-07 02:27:43 +0200

    expose NODE_ENV to jade via build script

--- build.sh
+++ build.sh
@@ -6,5 +6,5 @@ PATH=./node_modules/.bin:$PATH
 
 browserify --entry client.js | uglifyjs --mangle --compress warnings=false > static/-.js
 stylus --compress < style.styl > static/-.css
-jade --no-debug -O '{"url":"'"$URL"'", "static_url":"'"$STATIC_URL"'", "version": "'"$VER"'"}' < index.jade > static/index.html
+jade --no-debug -O '{"url":"'"$URL"'", "static_url":"'"$STATIC_URL"'", "version": "'"$VER"'", "settings":{"env":"'"$NODE_ENV"'"}}' < index.jade > static/index.html
 
