da35e1ff253c7ea9d1731d48a34b10d890c2c309
Nadav Ivgi <nadav@shesek.info>
2016-11-16 09:47:53 +0200

    "Try a new wallet" should close the error modal

--- views/error-dialog.js
+++ views/error-dialog.js
@@ -7,16 +7,17 @@ module.exports = error =>
           <h4 class="modal-title">Oops! Something went wrong</h4>
         </div>
         <div class="modal-body">
-          <p>Lightning is still an experimental technology at its early stages,
+          <p>Lightning is still an experimental technology at its early stages, as is this demo,
              and errors still do happen. Sorry about that!</p>
           <p>Would  you like to:</p>
         </div>
         <div class="modal-footer">
           <button type="button" class="btn btn-default" data-dismiss="modal">Close dialog</button>
-          <a href="#/new" class="btn btn-primary">Try a new wallet</a>
+          <a href="#new" class="btn btn-primary">Try a new wallet</a>
         </div>
       </div>
     </div>
   </div>`)
 
   .modal()
+  .on('click', 'a[href="#new"]', function(e) { $(e.delegateTarget).modal('hide') })
