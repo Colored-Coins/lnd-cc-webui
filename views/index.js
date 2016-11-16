b916aa878928cd416da78a13d3f0757a7edca7e6
Nadav Ivgi <nadav@shesek.info>
2016-11-16 14:45:39 +0200

    add some text to homepage

--- views/index.js
+++ views/index.js
@@ -1,7 +1,10 @@
-import { header, div, a } from '@cycle/dom'
+import { header, div, h2, p, a } from '@cycle/dom'
 
-module.exports = header(
-  div('.container.center', [
-    a('.new-wallet.btn.btn-default.btn-lg', { href: '#new' }, 'Create Lightning wallet')
+module.exports = div([
+  header(div('.container.center', h2('Colu Lightning Demo')))
+, div('.container', [
+    p(null, 'Click the button below to open a demo wallet with some free play money, and start sending and receiving payments!')
+  , p([ 'Visit our ', a({ href: '/faq' }, 'FAQ'), ' for more details.' ])
+  , a('.new-wallet.btn.btn-primary.btn-lg', { href: '#new' }, 'Create Lightning wallet')
   ])
-)
+])
